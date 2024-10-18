06c17024d7da51b4a3621bee0d41687937b69f39
Athul Bhandary <athulbhandary28@gmail.com>
2024-10-18 11:47:49 +0530

    Added auth.js

--- code/frontend/src/app/layout.tsx
+++ code/frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 import Navbar from "@/components/Navbar/Navbar";
 
 const geistSans = localFont({
@@ -29,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
+        <SessionProvider>
+          <Navbar />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
--- code/frontend/src/components/Navbar/Navbar.tsx
+++ code/frontend/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Navbar() {
+  const { data: session } = useSession();
+  const userName = session?.user?.name;
   return (
     <>
       <nav>This is navbar</nav>
+      {/* <SignIn /> */}
+      <p>{userName}</p>
+      <button onClick={() => signIn("google")}>SignIn</button>
     </>
   );
 }
